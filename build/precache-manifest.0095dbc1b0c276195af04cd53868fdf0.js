27bd0dabee8c13fb70e6a58be453f45aada93077
Kalaiarasan Seetharaman <kalaiarasanseetharaman@gmail.com>
2020-04-05 23:46:24 +0530

    Add files via upload

---
+++ build/precache-manifest.0095dbc1b0c276195af04cd53868fdf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d39505b662ae2adfa51b60d6788256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321ddbbc69782b410c68",
+    "url": "/static/css/2.d9183e08.chunk.css"
+  },
+  {
+    "revision": "1c6f6136d20f5067a069",
+    "url": "/static/css/main.5027c140.chunk.css"
+  },
+  {
+    "revision": "321ddbbc69782b410c68",
+    "url": "/static/js/2.263e6863.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.263e6863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6f6136d20f5067a069",
+    "url": "/static/js/main.03079f79.chunk.js"
+  },
+  {
+    "revision": "2f36df705ac37a44d470",
+    "url": "/static/js/runtime-main.2e7fe822.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
